62e95ec69a1dfb3af3b143c3cf898e6eda30c040
mariaamoon008@gmail.com <mariaamoon98@gmail.com>
2025-05-27 10:14:48 +0330

    hp improvement

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -7,7 +7,7 @@ public class Item {
 
     public void hpUp() {
         if (player.gethp() < 100 && player.gethp() > 80) {
-            player.setHp(320);
+            player.setHp(100);
         } else if (player.gethp() <= 80 && player.gethp() > 0) {
             player.setHp(player.gethp() + 20);
         }
